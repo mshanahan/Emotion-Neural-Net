d3ab8f2181d062343bce031603b63c2d0c03e951
Chulwoo Mike Pack <chulwoo.pack@gmail.com>
2018-02-17 03:10:49 -0600

    Add files via upload

--- part1/main.py
+++ part1/main.py
@@ -1,137 +1,147 @@
-#author: Michael Shanahan 42839964
-import model
-import util
 import tensorflow as tf
 import numpy as np
+import os
+import util
+import model
 
-#code to set flags by Paul Quint
+# flags
 flags = tf.app.flags
-flags.DEFINE_string('data_dir', '/work/cse496dl/shared/homework/02/EMODB-German/', 'directory where MNIST is located')
-flags.DEFINE_string('save_dir', 'homework_2', 'directory where model graph and weights are saved')
+flags.DEFINE_string('data_dir', '/work/cse496dl/shared/homework/02/EMODB-German/', 'directory where FMNIST is located')
+flags.DEFINE_string('save_dir', '/work/cse496dl/cpack/Assignment_2/models/1', 'directory where model graph and weights are saved')
 flags.DEFINE_integer('batch_size', 32, '')
-flags.DEFINE_integer('max_epoch_num', 100, '')
+flags.DEFINE_integer('max_epoch_num', 2, '')
+flags.DEFINE_integer('tol_epoch_num', 2, '')
+flags.DEFINE_float('dropout', 0.5, '')
+flags.DEFINE_bool('is_testing', False, '')
 FLAGS = flags.FLAGS
 
 def main(argv):
-  #load data
-  valid_data_1 = np.load(FLAGS.data_dir + 'test_x_1.npy')
-  valid_data_2 = np.load(FLAGS.data_dir + 'test_x_2.npy')
-  valid_data_3 = np.load(FLAGS.data_dir + 'test_x_3.npy')
-  valid_data_4 = np.load(FLAGS.data_dir + 'test_x_4.npy')
-  valid_data = [valid_data_1, valid_data_2, valid_data_3, valid_data_4]
-  
-  valid_labels_1 = np.load(FLAGS.data_dir + 'test_y_1.npy')
-  valid_labels_2 = np.load(FLAGS.data_dir + 'test_y_2.npy')
-  valid_labels_3 = np.load(FLAGS.data_dir + 'test_y_3.npy')
-  valid_labels_4 = np.load(FLAGS.data_dir + 'test_y_4.npy')
-  valid_labels = [valid_labels_1, valid_labels_2, valid_labels_3, valid_labels_4]
-  
-  train_data_1 = np.load(FLAGS.data_dir + 'train_x_1.npy')
-  train_data_2 = np.load(FLAGS.data_dir + 'train_x_2.npy')
-  train_data_3 = np.load(FLAGS.data_dir + 'train_x_3.npy')
-  train_data_4 = np.load(FLAGS.data_dir + 'train_x_4.npy')
-  train_data = [train_data_1, train_data_2, train_data_3, train_data_4]
-  
-  train_labels_1 = np.load(FLAGS.data_dir + 'train_y_1.npy')
-  train_labels_2 = np.load(FLAGS.data_dir + 'train_y_2.npy')
-  train_labels_3 = np.load(FLAGS.data_dir + 'train_y_3.npy')
-  train_labels_4 = np.load(FLAGS.data_dir + 'train_y_4.npy')
-  train_labels = [train_labels_1, train_labels_2, train_labels_3, train_labels_4]
-  
-#  print(str(valid_data_1[0*32:(0+1)*32, :]))
-#  print(str(valid_labels_1[0*32:(0+1)*32, :]))
-  
-  #count data
-  valid_count = [valid_data[0].shape[0], valid_data[1].shape[0], valid_data[2].shape[0], valid_data[3].shape[0]]
-  train_count = [train_data[0].shape[0], train_data[1].shape[0], train_data[2].shape[0], train_data[3].shape[0]]
+    images_1 = np.load(FLAGS.data_dir + 'train_x_1.npy')
+    images_2 = np.load(FLAGS.data_dir + 'train_x_2.npy')
+    images_3 = np.load(FLAGS.data_dir + 'train_x_3.npy')
+    images_4 = np.load(FLAGS.data_dir + 'train_x_4.npy')
+    train_images = [images_1, images_2, images_3, images_4]
+    
+    labels_1 = np.load(FLAGS.data_dir + 'train_y_1.npy')
+    labels_2 = np.load(FLAGS.data_dir + 'train_y_2.npy')
+    labels_3 = np.load(FLAGS.data_dir + 'train_y_3.npy')
+    labels_4 = np.load(FLAGS.data_dir + 'train_y_4.npy')
+    train_labels = [labels_1, labels_2, labels_3, labels_4]
 
-  #specify model
-  input_placeholder = tf.placeholder(tf.float32, [None,16641], name='input_placeholder')
-  conv_layer = model.convolutional_layer(input_placeholder)
-  linear_layer = model.linear_layer(conv_layer)
-  output_layer = tf.layers.dense(linear_layer, 7, name = 'output_layer')
-  my_network = tf.identity(output_layer,name='output')
+    # Reshaping
+    #images = np.reshape(images,[-1,129,129,1])         
 
-  #define classification loss
-  #code adapted from Paul Quint's hackathon 3
-  REG_COEFF = 0.0001
-  labels = tf.placeholder(tf.float32, [None, 7], name='labels')
-  cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=my_network)
-  confusion_matrix_op = tf.confusion_matrix(tf.argmax(labels, axis=1), tf.argmax(my_network, axis=1), num_classes=7)
-  regularization_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-  total_loss = cross_entropy + REG_COEFF * sum(regularization_losses)
+    #train_images, test_images = split_data(images, 0.9)
+    #train_labels, test_labels = split_data(labels, 0.9)
+    # Not sure this randomized splitting is acceptable with the use of the k-fold validation.
+    #train_images, validation_images = util.split_data(images, 0.9)
+    #train_labels, validation_labels = util.split_data(labels, 0.9)
+    
+    
+    images_1 = np.load(FLAGS.data_dir + 'test_x_1.npy')
+    images_2 = np.load(FLAGS.data_dir + 'test_x_2.npy')
+    images_3 = np.load(FLAGS.data_dir + 'test_x_3.npy')
+    images_4 = np.load(FLAGS.data_dir + 'test_x_4.npy')
+    valid_images = [images_1, images_2, images_3, images_4]
+    
+    labels_1 = np.load(FLAGS.data_dir + 'test_y_1.npy')
+    labels_2 = np.load(FLAGS.data_dir + 'test_y_2.npy')
+    labels_3 = np.load(FLAGS.data_dir + 'test_y_3.npy')
+    labels_4 = np.load(FLAGS.data_dir + 'test_y_4.npy')
+    valid_labels = [labels_1, labels_2, labels_3, labels_4]
 
-  #set up training and saving
-  #code adapted from Paul Quint's hackathon 3
-  global_step_tensor = tf.get_variable('global_step', trainable=False, shape=[], initializer=tf.zeros_initializer)
-  optimizer = tf.train.AdamOptimizer()
-  train_op = optimizer.minimize(total_loss, global_step=global_step_tensor)
-  saver = tf.train.Saver()
-  sum_cross_entropy = tf.reduce_mean(cross_entropy)
-  
-  EPOCHS_BEFORE_STOPPING = 12
-  best_epoch = 0
-  best_valid_ce = 0
-  best_train_ce = 0
-  best_classification_rate = 0
-  epochs_since_best = 0
-  
-  #run the actual training
-  #code adapted from Paul Quint's hackathon 3
-  with tf.Session() as session:
-    session.run(tf.global_variables_initializer())
-    batch_size = FLAGS.batch_size
-    for epoch in range(FLAGS.max_epoch_num):
-      print("################### EPOCH " + str(epoch) + " #####################")
-      print("##################################################\n")
-      
-      # run gradient steps and report mean loss on train data
-      # !!!!!!!!!!!!!!!!
-      # TODO: Set this up for 4-fold cross validation (right now it just uses the first set of data)
-      ce_vals = []
-      conf_mxs = []
-      for i in range(valid_count[0] // batch_size):
-        batch_data = valid_data[0][i*batch_size:(i+1)*batch_size, :]
-        batch_labels = valid_labels[0][i*batch_size:(i+1)*batch_size]
-        #batch_labels = util.white_hot(batch_labels)
-        valid_ce, conf_matrix = session.run([sum_cross_entropy, confusion_matrix_op], {input_placeholder: batch_data, labels: batch_labels})
-        ce_vals.append(valid_ce)
-        conf_mxs.append(conf_matrix)
-      avg_valid_ce = sum(ce_vals) / len(ce_vals)
-      print('VALID CROSS ENTROPY: ' + str(avg_valid_ce))
-      print('VALIDATION CONFUSION MATRIX:')
-      print(str(sum(conf_mxs)))
-      classification_rate = util.classification_rate(sum(conf_mxs),7)
-      print('VALIDATION CLASSIFICATION RATE:' + str(classification_rate))
-      
-      ce_vals = []
-      for i in range(train_count[0] // batch_size):
-        batch_data = train_data[0][i*batch_size:(i+1)*batch_size, :]
-        batch_labels = train_labels[0][i*batch_size:(i+1)*batch_size]
-        #batch_labels = util.white_hot(batch_labels)
-        _, train_ce = session.run([train_op, sum_cross_entropy], {input_placeholder: batch_data, labels: batch_labels})
-        ce_vals.append(train_ce)
-      avg_train_ce = sum(ce_vals) / len(ce_vals)
-      print('TRAIN CROSS ENTROPY: ' + str(avg_train_ce))
-      
-      epochs_since_best += 1
-      
-      if(best_valid_ce < avg_valid_ce): #tracking best
-        best_valid_ce = avg_valid_ce
-        best_train_ce = avg_train_ce
-        best_epoch = epoch
-        best_classification_rate = classification_rate
-        epochs_since_best = 0
-        saver.save(session, "/work/cse496dl/mshanaha/homework_1/homework_1-0")
-        print("BEST FOUND")
+    train_num_examples = [train_images.shape[0], train_images.shape[1], train_images.shape[2], train_images.shape[3]]
+    valid_num_examples = [valid_images.shape[0], valid_images.shape[1], valid_images.shape[2], valid_images.shape[3]] 
+
+    # specify the network
+    tf.reset_default_graph()
+    with tf.name_scope('Input_Placeholder') as scope:
+        x = tf.placeholder(shape=[None, 16641], name='input_placeholder', dtype=tf.float32)
+        y = tf.placeholder(shape=[None, 7], name='output', dtype=tf.float32)
+
+    with tf.name_scope('3_layer_cnn_model') as scope:
+        output_layer = model.conv_block(x, [32,64,128], [3,3,3], tf.nn.relu, 0.5)
+        logits = tf.identity(output_layer,name='output_hmm')
+
+    # Calculate loss
+    with tf.name_scope('Loss') as scope:
+        loss_op = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits)
+        reduce_mean = tf.reduce_mean(loss_op)
+
+    # Training optimizer
+    with tf.name_scope('Train') as scope:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001, name='Adam_Op')
+        train_op = optimizer.minimize(loss=reduce_mean,
+                             global_step=tf.train.get_global_step())
+
+    global_step_tensor = tf.get_variable('global_step', trainable=False, shape=[], initializer=tf.zeros_initializer) # This is for allowing to keep track how far it is optimized so far.
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        tf.summary.FileWriter("/work/cse496dl/cpack/Assignment_2/logs", tf.get_default_graph()).close()
         
-      if(epochs_since_best >= EPOCHS_BEFORE_STOPPING): #early stopping
-        print("EARLY STOP")
-        break
+        # run training
+        min_validation_ce = float('INF')
+        idx_min_validation_ce = 0
         
-      print("\n##################################################")
-      
-    print('Best Epoch: ' + str(best_epoch))
+        for epoch in range(FLAGS.max_epoch_num):
+            # Early stopping
+            if epoch > 0 and min_validation_ce < avg_validation_ce_vals[-1] and epoch-idx_min_validation_ce>FLAGS.tol_epoch_num:
+                print('No more loss reduction on validation set over ' + str(FLAGS.tol_epoch_num) + ' epochs.')
+                print('Training process is terminated..')
+                break
+                
+            print('Epoch: ' + str(epoch))
 
-if __name__ == "__main__":
+            # run gradient steps and report mean loss on train data
+            ce_vals = []
+            for i in range(train_num_examples // FLAGS.batch_size):
+                batch_xs = train_images[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size, :]
+                batch_ys = train_labels[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size, :]
+                #if epoch%2 == 0:
+                #    s, _ = sess.run([merged_summary,_], {x: batch_xs, y: batch_ys})
+                #    writer.add_summary(s, epoch)
+                _, train_ce = sess.run([train_op, reduce_mean], {x: batch_xs, y: batch_ys})
+                ce_vals.append(train_ce)
+                avg_train_ce = sum(ce_vals) / len(ce_vals)
+            print('TRAIN CROSS ENTROPY: ' + str(avg_train_ce))
+            
+            # run validation set
+            ce_vals = []
+            avg_validation_ce_vals = []
+            for i in range(validation_num_examples // FLAGS.batch_size):
+                batch_xs = validation_images[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size, :]
+                batch_ys = validation_labels[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size, :]
+                validation_ce = sess.run(reduce_mean, {x: batch_xs, y: batch_ys})
+                ce_vals.append(validation_ce)
+            avg_validation_ce = sum(ce_vals) / len(ce_vals)
+            avg_validation_ce_vals.append(avg_validation_ce)
+            # update minimum validation_ce
+            if avg_validation_ce < min_validation_ce:
+                min_validation_ce = avg_validation_ce
+                idx_min_validation_ce = epoch
+                #path_prefix = saver.save(sess, os.path.join(FLAGS.save_dir, "fmnist_inference_chulwoo"), global_step=global_step_tensor)
+                print('New model has been saved!')
+            print('VALIDATION CROSS ENTROPY: ' + str(avg_validation_ce))
+
+            # report mean test loss
+        
+            FLAGS.dropout = 0
+            ce_vals = []
+            conf_mxs = []
+        #for i in range(test_num_examples // FLAGS.batch_size):
+        #    batch_xs = test_images[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size, :]
+        #    batch_xs = np.reshape(batch_xs,[-1,28,28,1])
+        #    batch_ys = test_labels[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size, :]
+        #    test_ce, conf_matrix = sess.run([reduce_mean, confusion_matrix_op], {x: batch_xs, y: batch_ys})
+        #    ce_vals.append(test_ce)
+        #    conf_mxs.append(conf_matrix)
+        #avg_test_ce = sum(ce_vals) / len(ce_vals)
+        #print('TEST CROSS ENTROPY: ' + str(avg_test_ce))
+        #print('TEST CONFUSION MATRIX:')
+        #print(str(sum(conf_mxs)))
+
+if __name__ == '__main__':
     tf.app.run()
+        
+        
--- part1/model.py
+++ part1/model.py
@@ -3,9 +3,12 @@ import numpy as np
 import os
 
 def conv_block(inputs, filters, kernel_size, activation, dropout):
+    with tf.name_scope('Input_reshaped') as scope:
+        reshaped_inputs = tf.reshape(inputs, [-1, 129, 129, 1])
+    
     # Conv & pooling layer 1
     with tf.name_scope('Conv_Pool_1') as scope:
-        conv1 = tf.layers.conv2d(inputs=inputs,
+        conv1 = tf.layers.conv2d(inputs=reshaped_inputs,
                          filters=filters[0],
                          kernel_size=kernel_size[0],
                          padding="same",
@@ -30,26 +33,26 @@ def conv_block(inputs, filters, kernel_size, activation, dropout):
                                name='pool_2')
                                
     # Conv & pooling layer 3
-    #with tf.name_scope('Conv_Pool_3') as scope:
-    #    conv3 = tf.layers.conv2d(inputs=pool2,
-    #                    filters=filters[2],
-    #                    kernel_size=kernel_size[2],
-    #                    padding="same",
-    #                    activation=activation,
-    #                    name='conv_3')
-    #    pool3 = tf.layers.max_pooling2d(inputs=conv3,
-    #                            pool_size=[2, 2],
-    #                            strides=2,
-    #                            name='pool_3')
+    with tf.name_scope('Conv_Pool_3') as scope:
+        conv3 = tf.layers.conv2d(inputs=pool2,
+                        filters=filters[2],
+                        kernel_size=kernel_size[2],
+                        padding="same",
+                        activation=activation,
+                        name='conv_3')
+        pool3 = tf.layers.max_pooling2d(inputs=conv3,
+                                pool_size=[2, 2],
+                                strides=2,
+                                name='pool_3')
 
     # Logit layer
-    flat = tf.reshape(pool2,[-1, 32*32*64])
-    #flat = tf.reshape(pool3,[-1, 16*16*128])
+    #flat = tf.reshape(pool2,[-1, 32*32*64])
+    flat = tf.reshape(pool3,[-1, 16*16*128])
     #flat = tf.layers.dense(flat,1024)
     #flat = tf.layers.dense(flat, 32768, name='dense_1')
-    flat = tf.layers.dense(flat, 30198, name='dense_1')
+    #flat = tf.layers.dense(flat, 30198, name='dense_1')
     dropout = tf.layers.dropout(inputs=flat,
                            rate=dropout, name='dropout')
-    logits = tf.layers.dense(dropout, units=7, name='logits')
+    output = tf.layers.dense(dropout, units=7, name='output')
         
-    return logits
+    return output
